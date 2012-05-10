41d8ac660b6b9ea1927cc1426484726d64216433
Yogesh Pandit <yogeshp08@gmail.com>
2012-05-10 16:28:50 -0500

    Adding the MOD/SGD library

---
+++ lib/MOD/SGD/StockCenterInventoryDual.pm
@@ -0,0 +1,16 @@
+package MOD::SGD::StockCenterInventoryDual;
+
+use strict;
+use warnings;
+
+use base 'DBIx::Class::Core';
+
+__PACKAGE__->table("Dual");
+__PACKAGE__->add_columns(
+    "dummy",
+    {   data_type   => "NUMBER",
+        is_nullable => 0,
+        size        => 1,
+    }
+);
+1;
