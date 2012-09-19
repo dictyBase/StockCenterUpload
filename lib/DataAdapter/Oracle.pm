b3a21b92c8fb30e7064df5fbc6f8d20f99ece4da
Yogesh Pandit <yogeshp08@gmail.com>
2012-09-19 11:16:36 -0500

    Commented print in terminal after loading

--- lib/DataAdapter/Oracle.pm
+++ lib/DataAdapter/Oracle.pm
@@ -53,46 +53,48 @@ has 'schema' => (
 sub insert {
     my ( $self, $row ) = @_;
     my $inventory_data;
-    my $strain_row =
-      $self->schema->resultset('StockCenter')
-      ->search( { strain_name => $row->strain_desc }, { rows => 1 } )->single;
+    my $strain_row
+        = $self->schema->resultset('StockCenter')
+        ->search( { strain_name => $row->strain_desc }, { rows => 1 } )
+        ->single;
 
     if ( !$strain_row ) {
         return;
     }
 
-    my $dual_rs = $self->schema->resultset('StockCenterInventoryDual')->search(
+    my $dual_rs
+        = $self->schema->resultset('StockCenterInventoryDual')->search(
         undef,
-        {
-            select => ['STOCK_CENTER_INVENTORY_SEQ.nextval'],
+        {   select => ['STOCK_CENTER_INVENTORY_SEQ.nextval'],
             as     => ['id'],
         }
-    );
+        );
 
-    my $storage_date =
-        $row->storage_date->day . '-'
-      . uc $row->storage_date->month_abbr . '-'
-      . $row->storage_date->year;
+    my $storage_date
+        = $row->storage_date->day . '-'
+        . uc $row->storage_date->month_abbr . '-'
+        . $row->storage_date->year;
     my $data = $dual_rs->next;
     push @$inventory_data,
-      [
+        [
         $data->get_column('id'), $strain_row->id,
         $row->location,          $storage_date,
         $row->num_vials,         $row->color,
         $row->stored_by,         'CGM_DDB_KERRY',
         $row->comments
-      ];
+        ];
 
     if ( scalar @$inventory_data > 0 ) {
         unshift @$inventory_data, [
             qw/id strain_id location storage_date no_of_vials color
-              stored_by created_by storage_comments/
+                stored_by created_by storage_comments/
         ];
         $self->schema->txn_do(
             sub {
                 $self->schema->resultset('StockCenterInventory')
-                  ->populate($inventory_data);
-                print " Loaded ", scalar @$inventory_data - 1, " data \n ";
+                    ->populate($inventory_data);
+
+                #print " Loaded ", scalar @$inventory_data - 1, " data \n ";
             }
         );
     }
