be21b0839ed106b6e4ada8d4c4fa0365452ca6e1
Yogesh Pandit <yogeshp08@gmail.com>
2012-09-20 13:34:33 -0500

    Using temp file for uploads

--- lib/StockCenter/Upload.pm
+++ lib/StockCenter/Upload.pm
@@ -7,29 +7,30 @@ use Mojo::Base 'Mojolicious::Controller';
 use Data::Dump qw/pp/;
 use StockCenter::Parser;
 use DBCon::Uploader;
+use File::Temp;
 
-#
 sub new_record {
 }
 
-#
 sub create {
     my ($self) = @_;
     my $db = $self->upload_db;
-    my $sth =
-      $db->prepare("INSERT INTO uploaded_file(name, size) VALUES(?, ?)");
+    my $sth
+        = $db->prepare("INSERT INTO uploaded_file(name, size) VALUES(?, ?)");
     my ($upload) = @{ $self->req->uploads };
     my $filename = $upload->filename;
     $sth->execute( $filename, $upload->size );
     my $id = $db->last_insert_id( "", "", "", "" );
-    $upload->move_to(
-        $self->app->home->rel_file( "uploads/" . $id . "_" . $filename ) );
+    my $temp_file = File::Temp->new(SUFFIX => '.dat');
+    $upload->move_to($temp_file);
+	#$upload->move_to($self->app->home->rel_file( "uploads/" . $id . "_" . $filename ) );
     my $headers = $self->res->headers;
     $headers->content_type('text/plain');
-    $headers->location( $self->url_for("uploads/$id")->to_abs );
+	#$headers->location( $self->url_for("uploads/$id")->to_abs );
 
-    my $file = $self->app->home->rel_file( "uploads/" . $id . "_" . $filename );
-    my $parser  = StockCenter::Parser->new( file => $file );
+	my $file = $temp_file->filename;
+	# my $file  = $self->app->home->rel_file( "uploads/" . $id . "_" . $filename );
+    my $parser = StockCenter::Parser->new( file => $file );
     my $adapter = $self->adapter;
     while ( $parser->has_next() ) {
         my $row = $parser->next();
@@ -39,7 +40,6 @@ sub create {
     return;
 }
 
-#
 sub index {
 }
 
@@ -49,16 +49,16 @@ sub search {
     my $limit  = $self->param('iDisplayLength');
     my $offset = $self->param('iDisplayStart');
     my $db     = $self->upload_db;
-    my $sth    = $db->prepare(
+    my $sth
+        = $db->prepare(
         "SELECT name, size, created_at FROM uploaded_file LIMIT $offset,$limit"
-    );
+        );
     my $cth     = $db->prepare("SELECT count(id) FROM uploaded_file");
     my ($total) = $db->selectrow_array($cth);
     my $arr     = $db->selectall_arrayref($sth);
 
     $self->render_json(
-        {
-            sEcho                => $self->param('sEcho'),
+        {   sEcho                => $self->param('sEcho'),
             iTotalRecords        => $total,
             iTotalDisplayRecords => $total,
             aaData               => $arr
